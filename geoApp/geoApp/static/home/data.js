aeef56e27c748eca6eb62a999b2ee726d1ca2f5a
cecil <cecilghimire@gmail.com>
2022-10-30 15:15:32 +0545

    added index.html leaflet  file and other static file added

---
+++ geoApp/geoApp/static/home/data.js
@@ -0,0 +1,60 @@
+var data={
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {name:"HOME"},
+        "geometry": {
+          "coordinates": [
+            22.00760854882401,
+            7.682354176303264
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            27.506574734803678,
+            9.700376344886791
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            18.01044023690622,
+            16.142206114252673
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            30.98832454801027,
+            7.692694234716512
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            36.058318812009645,
+            9.482365267021791
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
